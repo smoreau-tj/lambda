9e3c77bd9b5add791523f7f5e6d24bba9ca3ee86
Pooja Patil <pooja.patil@tommyjohnwear.com>
2021-06-23 15:17:39 -0500

    resolved email variable error and updated sendToIterable API call

--- iterable-opt-out.js
+++ iterable-opt-out.js
@@ -4,21 +4,32 @@ exports.handler = async (event) => {
     // TODO implement
     let body = event
     let statusCode
-    //let { userEmail, subscriptionGroupId } = { userEmail, subscriptionGroupId, ...event }
+    let userEmail = event.userEmail
+    let subscriptionGroupId = event.subscriptionGroupId
     let error
     const subscriptionGroup = "messageType"
-    //let url = `https://api.iterable.com/api/subscriptions/${subscriptionGroup}/${subscriptionGroupId}/user/${userEmail}`
-    // if(event.email == null || undefined && event.subscriptionGroupId == null || undefined ) {
-    //     statusCode = 400
-    //     body = "No Email was provided"
-    // } else {
-    //     statusCode = 200
-    //     body = event.email
-    // }
-    async function sendToIterable (params) {
+    const apiKey = event.apiKey
+    let url = `https://api.iterable.com/api/subscriptions/${subscriptionGroup}/${subscriptionGroupId}/user/${userEmail}`
+    console.log(url);
+    if(userEmail == null || undefined && event.subscriptionGroupId == null || undefined ) {
+        statusCode = 400
+         body = "No Email was provided"
+     } else {
+        statusCode = 200
+        body = userEmail
+        sendToIterable(url)
+     }
+    async function sendToIterable (url) {
       try {
-        // const response = await fetch(url)
-        // const data = await response.json()
+        const response = await fetch(url, {
+          method: 'DELETE',
+          headers: {
+              'Content-type': 'application/json',
+              'api-key': apiKey
+          }
+        });
+        const data = await response.json()
+        console.log(data);
       }catch(err) { error = err }
         
     }
