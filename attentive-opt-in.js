4d0a34ddc00d7a3a2b1e2f663fc76a4d6942a9ae
Serge M <serge@tommyjohnwear.com>
2021-06-24 11:16:11 -0400

    attentive-opt-in

---
+++ attentive-opt-in.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+
+const url = `https://rickandmortyapi.com/api/character`
+
+const getCharacters = async () => {
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log('something went wrong: ', error)
+  }
+}
+async function main(event) {
+
+  let statusCode
+  let body = event.phone
+  const data = await getCharacters()
+  
+  statusCode
+  let response = {
+    statusCode,
+    body: JSON.stringify(body)
+  }
+
+  return response
+}
+
+
+
+exports.handler = main
