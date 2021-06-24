1472fb8994700e11433800774b36194b59238380
Pooja Patil <pooja.patil@tommyjohnwear.com>
2021-06-24 12:57:06 -0500

    changes for returing iterable response

--- iterable-opt-out.js
+++ iterable-opt-out.js
@@ -1,42 +1,68 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
-    // TODO implement
-    let body = event
-    let statusCode
-    let userEmail = event.userEmail
-    let subscriptionGroupId = event.subscriptionGroupId
-    let error
-    const subscriptionGroup = "messageType"
-    const apiKey = event.apiKey
-    let url = `https://api.iterable.com/api/subscriptions/${subscriptionGroup}/${subscriptionGroupId}/user/${userEmail}`
-    console.log(url);
-    if(userEmail == null || undefined && event.subscriptionGroupId == null || undefined ) {
-        statusCode = 400
-         body = "No Email was provided"
-     } else {
+    let statusCode;
+    let userEmail = event.userEmail;
+    let email = '';
+    let subscriptionGroupId = event.subscriptionGroupId;
+    let error;
+    const subscriptionGroup = "messageType";
+    const apiKey = event.apiKey;
+    let responseFromIterable;
+    let iterableErrorMsg = '';
+    let iterableErrorCode = '';
+    let validateErrorMsg = '';
+    let url = `https://api.iterable.com/api/subscriptions/${subscriptionGroup}/${subscriptionGroupId}/user/${userEmail}`;
+
+    if (userEmail == null || undefined && subscriptionGroupId == null || undefined) {
+        statusCode = 400;
+        validateErrorMsg = "Neither Email nor Subscription Group Id was provided";
+    } else if (userEmail == null || undefined || userEmail == '') {
+        statusCode = 400;
+        validateErrorMsg = "No email was provided";
+    } else if (subscriptionGroupId == null || undefined || subscriptionGroupId == '') {
+        statusCode = 400;
+        validateErrorMsg = "No Subscription Group Id was provided";
+    } else {
         statusCode = 200
-        body = userEmail
-        sendToIterable(url)
-     }
-    async function sendToIterable (url) {
-      try {
-        const response = await fetch(url, {
-          method: 'DELETE',
-          headers: {
-              'Content-type': 'application/json',
-              'api-key': apiKey
-          }
-        });
-        const data = await response.json()
-        console.log(data);
-      }catch(err) { error = err }
-        
+        responseFromIterable = await sendToIterable(url);
+        iterableErrorMsg = responseFromIterable.msg;
+        iterableErrorCode = responseFromIterable.code;
+        if (iterableErrorCode == 'Success') {
+          email = userEmail
+        } else {
+          email = '';
+        }
+    }
+
+    async function sendToIterable(url) {
+        try {
+            const response = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json',
+                    'api-key': apiKey
+                }
+            });
+            const data = await response.json()
+            const sendIterbaleResponse = {
+                msg: data.msg,
+                code: data.code
+            };
+            return sendIterbaleResponse;
+
+        } catch (err) {
+            error = err
+        }
     }
     const response = {
         statusCode,
-        body: JSON.stringify(body),
-        error 
+        validateErrorMsg,
+        email,
+        iterableErrorMsg,
+        iterableErrorCode,
+        error
     };
     return response;
+
 };
